0532d9f32093df5ad620164d9a90c08c7d968722
Kasia_Gorecka <katarzyna.gorecka.janiszewska@gmail.com>
2023-04-02 10:28:13 +0200

    pierwsze zmiany

---
+++ october_OOP_exercises/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace october_OOP_exercises
+{
+    internal class OrderItem  //linijka na paragonie: cena jednostkowa i wartośc i ilośc i nazwa produktu
+    {
+         private string productName;
+         private decimal price;
+         private int quantity;
+
+        public OrderItem(string productName , decimal price, int quantity)
+        {
+            this.productName = productName;
+            this.price = price;
+            this.quantity = quantity;
+
+        }
+
+        public override string ToString()
+        {
+            return productName;
+        }
+    }
+}
--- october_OOP_exercises/Program.cs
+++ october_OOP_exercises/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            OrderItem orderItem = new OrderItem("mleko", 3.2m ,4);
+            Console.WriteLine(orderItem);
         }
     }
 }
